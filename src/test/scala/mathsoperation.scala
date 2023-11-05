7f738aa1d22307cf08fd1bd235d53bdcdbaf5e4f
niteshanjane <nitesh.anjan@gmail.com>
2023-11-05 15:02:54 +0530

    Maths operations

---
+++ src/test/scala/mathsoperation.scala
@@ -0,0 +1,31 @@
+object mathsoperation {
+
+  #Function to perform addition
+
+  def add(num1, num2):
+
+  return num1 + num2
+
+  #Function to perform subtraction
+
+  def subtract(num1, num2):
+
+  return num1 - num2
+
+  #Function to perform multiplication
+
+  def multiply(num1, num2):
+
+  return num1 * num2
+
+  #Function to perform division
+
+  def divide(num1, num2):
+
+  if num2 != 0:
+  return num1 / num2
+  else:
+  return "Error! Division by zero."
+
+
+}
