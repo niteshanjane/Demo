82816e33ced756804e6ad856cb180d2108c8cec7
niteshanjane <nitesh.anjan@gmail.com>
2023-11-05 15:59:55 +0530

    count even numbers in range

---
+++ src/test/scala/problem12.scala
@@ -0,0 +1,7 @@
+object problem12 {
+  count = 0
+  for i in range(start, end+1):
+  if i % 2 == 0:
+    count += 1
+  print(f"Number of even numbers between {start} and {end}: {count_even_numbers(start, end)}")
+}
