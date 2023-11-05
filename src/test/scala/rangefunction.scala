601d904105b0a9b7834f33f0eed6ec9404990f1d
niteshanjane <nitesh.anjan@gmail.com>
2023-11-05 14:56:02 +0530

    Check the range of number

---
+++ src/test/scala/rangefunction.scala
@@ -0,0 +1,26 @@
+object rangefunction {
+
+
+  # Declare and initialize a number
+  num = 85
+
+  # Check whether the number is in range 0-100 or not
+  if num < 0 or num > 100:
+    print("Invalid input")
+  else:
+  # Check the range of the number and print the corresponding message
+  if num >= 90:
+    print("Super Smart")
+  elif num >= 80:
+    print("Smart")
+  elif num >= 70:
+    print("Smart enough")
+  elif num >= 60:
+    print("Just smart")
+  elif num >= 35:
+    print("Not smart")
+  else:
+  print("Dumb");
+
+
+}
