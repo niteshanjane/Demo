484b0a3a0d075ac3e49ebdb95842d2afe897fab1
niteshanjane <nitesh.anjan@gmail.com>
2023-11-05 16:39:29 +0530

    print the all alphabets by using character Variable

---
+++ src/test/scala/problem17.scala
@@ -0,0 +1,12 @@
+object problem17 {
+  # For lowercase alphabets
+  for i in range(97, 123):
+    print(chr(i), end=' ')
+
+  print()
+
+  # For uppercase alphabets
+  for i in range(65, 91):
+    print(chr(i), end=' ')
+
+}
